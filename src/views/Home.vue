442740d05f0e7bb0e8fa52ad31fa65ce692fdc53
Kévin <bernier.kevin59@gmail.com>
2020-05-01 11:04:15 +0200

    Init Home.vue / Order.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState(['board'])
+}
+</script>
+
+<style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+.column {
+  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
+.home {
+  @apply p-4 bg-teal-dark h-full overflow-auto;
+}
+
+.task-bg {
+  @apply pin absolute;
+  background: rgba(0,0,0,0.5);
+}
+</style>
