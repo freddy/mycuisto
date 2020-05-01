44f709d389fa7bd0500fc6daf9d19e80fb77a240
Kévin <bernier.kevin59@gmail.com>
2020-05-01 15:34:20 +0200

    Header / Footer

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+   <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <router-link
+      :to="{ name: 'home' }"
+      class="navbar-brand"
+      >
+        My Cuisto
+      </router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"
+          :class="{ active: true }" >
+          <router-link
+          :to="{ name: 'order' }"
+          class="nav-link"
+          >
+            Order
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.nav {
+  @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
+}
+</style>
