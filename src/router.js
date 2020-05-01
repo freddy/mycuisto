0f93a105609f4e377ab5743d2232317e954c001e
Kévin <bernier.kevin59@gmail.com>
2020-05-01 15:08:09 +0200

    Add router.js et store.js

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Order from './views/Order.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order
+    }
+  ]
+})
