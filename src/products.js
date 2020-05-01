1c669770bdd3d5dd379c782642d97fd45c6fe633
Freddy <freddy.dewaleyne@gmail.com>
2020-05-01 16:37:07 +0200

    products ids

--- src/products.js
+++ src/products.js
@@ -2,16 +2,19 @@ export default {
     name: 'catalogue',
     products: [
         {
+            id: 1,
             title: "Lasagne alla siciliana",
             description: "Sauce tomate, sauce bolognese, champignons, parmigiano, petits pois, œuf dur",
             price: "7.50"
         },
         {
+            id: 2,
             title: "Arancini al ragù",
             description: "Spécialité sicilienne boule de riz panée farcie de viande petit pois ,œuf dur et provolone",
             price: "4.50"
         },
         {
+            id: 3,
             title: "Arancini spinaci",
             description: "Boule de riz farci aux épinards et provolone",
             price: "4.50"
